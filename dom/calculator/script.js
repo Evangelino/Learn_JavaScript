555a066a0e32601214e6b68e13f57301295ffcad
Evangelino Ximenes <ximenesevang@gmail.com>
2022-02-11 16:24:33 +0200

    Update script.js
    Modelo calculator and variables that we will use.

---
+++ dom/calculator/script.js
@@ -0,0 +1,17 @@
+
+class Calculator {
+    constructor(previouseOperandTextElement,currentOperandTextElement){
+        this.previouseOperandTextElement = previouseOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+
+    };
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previouseOperandTextElement = document.querySelector('[data-previouse-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
